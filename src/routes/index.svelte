5c78ef5af71bb85d2980c1057aca64f15301f777
Kevin Lin <kevin.lin.p@gmail.com>
2022-01-07 18:40:38 -0500

    more login/logout flow

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,43 +1,71 @@
 <script context="module">
-	export const prerender = true;
+export const prerender = false;
 </script>
 
 <script>
-  import { handleIncomingRedirect, login, logout, fetch, getDefaultSession } from '@inrupt/solid-client-authn-browser'
-  import { getSolidDataset, saveSolidDatasetAt } from "@inrupt/solid-client";
+import {
+  handleIncomingRedirect,
+  login,
+  logout,
+  fetch,
+  getDefaultSession,
+  onLogin,
+  onLogout,
+  onSessionRestore,
+} from '@inrupt/solid-client-authn-browser'
+import { getSolidDataset, saveSolidDatasetAt } from "@inrupt/solid-client";
 
-  async function loginClicked(event) {
-    await handleIncomingRedirect();
+let loginStatus = null
 
-    if (!getDefaultSession().info.isLoggedIn) {
-      await login({
-        oidcIssuer: "https://broker.pod.inrupt.com",
-        redirectUrl: window.location.href,
-        clientName: "Eve Skill Queue Watch"
-      });
-    }
+async function loginClicked(event) {
+  if (!getDefaultSession().info.isLoggedIn) {
+    await login({
+      oidcIssuer: "https://broker.pod.inrupt.com",
+      redirectUrl: window.location.href,
+      clientName: "Eve Skill Queue Watch"
+    });
+  } else {
+    alert('never supposed to get here')
   }
+}
 
-  async function logoutClicked(event) {
-    await logout()
-    console.log(2)
-  }
+onLogin(() => {
+  loginStatus = true
+})
+onLogout(() => {
+  loginStatus = false
+})
+onSessionRestore(() => {
+  loginStatus = true
+})
+
+if (typeof window !== 'undefined') {
+  handleIncomingRedirect({restorePreviousSession: true});
+}
+
+async function logoutClicked(event) {
+  await logout()
+  console.log(2)
+}
 </script>
 
 <svelte:head>
-	<title>Eve Skill Queue Watch</title>
+  <title>Eve Skill Queue Watch</title>
   <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
 </svelte:head>
 
 <section>
   <div class="container">
-    <button on:click="{loginClicked}">
-      login 
-    </button>
-
-    <button on:click="{logoutClicked}">
-      logout
-    </button>
+    {#if loginStatus === true}
+      <button on:click="{logoutClicked}">
+        logout
+      </button>
+    {/if}
+    {#if loginStatus === false}
+      <button on:click="{loginClicked}">
+        login
+      </button>
+    {/if}
   </div>
 </section>
 

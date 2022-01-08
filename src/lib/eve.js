dc7dd4a6545ea6241092e210f85aee1ece53779f
Kevin Lin <kevin.lin.p@gmail.com>
2022-01-08 12:36:02 -0500

    eve SSO

--- src/lib/eve.js
+++ src/lib/eve.js
@@ -27,17 +27,33 @@ async function generateCodeChallenge(codeVerifier) {
   return base64ToBase64url(base64)
 }
 
-export async function authorize() {
+async function getQueryParams() {
   // https://datatracker.ietf.org/doc/html/rfc7636
   let codeVerifier = generateCodeVerifier()
   window.sessionStorage.setItem('codeVerifier', codeVerifier)
   let codeChallenge = await generateCodeChallenge(codeVerifier)
   console.log({codeVerifier, codeChallenge})
 
-  const queryParams = {
+  let state = generateCodeVerifier()
+  window.sessionStorage.setItem('state', state)
+
+  return {
     response_type: 'code',
     redirect_uri: 'http://localhost/eve/callback',
     client_id: '576dbe94b46e4174a96f8d4a63993293',
-    scope: 'esi-skills.read_skillqueue.v1,esi-clones.read_implants.v1',
+    scope: 'esi-skills.read_skillqueue.v1 esi-clones.read_implants.v1',
+    code_challenge: codeChallenge,
+    code_challenge_method: 'S256',
+    state,
   }
+}
+
+export async function authorize() {
+  const queryParams = await getQueryParams()
+  let url = 'https://login.eveonline.com/v2/oauth/authorize/?'
+  url = url + Object.keys(queryParams)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(queryParams[key])}`)
+    .join('&')
+  console.log({queryParams, url})
+  window.location = url
 }
